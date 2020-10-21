61736565789404894c6d1f49debc863b083d2b53
ShihChingYu <cyshih30@gmail.com>
2020-10-21 16:11:32 +0800

    add hello function

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+#' @title Print hello
+#'
+#' @description This function prints hello
+#'
+#' @param fname First name
+#' @param lname Last name
+#' @export
+#' @examples
+#' hello(fname="Tomas",lname="Greif")
+#' hello(fname="Your",lname="Name")
+hello <- function(fname, lname) {
+  cat(paste("Hello",fname,lname,"!"))
+}
